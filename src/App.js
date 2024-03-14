36fb92f3d41dcf52296d94b6f6445e71fdcb2006
Lavanyamadineni923 <mmadinenilavanya@gmail.com>
2024-03-14 11:49:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+// npm install axios
+// npm install react-router-dom
+// backend
+// creating command
+// npm Int
+
+// installation commands
+// json-server
+
+// start nodemon index.js
+// backend port 5000
+// apis-render
+
+
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Doctor from './Doctor';
+import Patient from './Patient';
+import Home from './Home';
+import Navigation from './Navigation';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App(){
+  
+   return(
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/doctors" element={<Doctor/>} />
+        <Route path="/patients" element={<Patient/>} />
+      </Routes>
+    </BrowserRouter>
+   )
+  }
 
 export default App;
